2735107f38974824ead72bdf82239c2f2d9d4b3e
cranderveldt <syghefish@gmail.com>
2018-12-08 11:00:45 -0500

    better ui

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -36438,12 +36438,104 @@ window.angular = _angular.default;
 
 var app = _angular.default.module('bstm', []);
 
-app.controller('MainController', ['$scope', function ($scope) {
+app.controller('MainController', ['$scope', '$q', '$timeout', function ($scope, $q, $timeout) {
   var vm = this;
+  vm.audioSrcName = 'Choose a Song';
   vm.start = false;
   vm.bpm = 152;
   vm.precision = .25;
+  vm.precisions = [{
+    value: 1,
+    label: 'Whole Notes'
+  }, {
+    value: .5,
+    label: 'Half Notes'
+  }, {
+    value: .25,
+    label: 'Quarter Notes'
+  }, {
+    value: .125,
+    label: 'Eighth Notes'
+  }, {
+    value: .0625,
+    label: 'Sixteenth Notes'
+  }];
   vm.notes = [];
+  vm.legalKeys = [{
+    x: 0,
+    y: 2,
+    key: 'r',
+    which: 114
+  }, {
+    x: 1,
+    y: 2,
+    key: 't',
+    which: 116
+  }, {
+    x: 2,
+    y: 2,
+    key: 'y',
+    which: 121
+  }, {
+    x: 3,
+    y: 2,
+    key: 'u',
+    which: 117
+  }, {
+    x: 0,
+    y: 1,
+    key: 'f',
+    which: 102
+  }, {
+    x: 1,
+    y: 1,
+    key: 'g',
+    which: 103
+  }, {
+    x: 2,
+    y: 1,
+    key: 'h',
+    which: 104
+  }, {
+    x: 3,
+    y: 1,
+    key: 'j',
+    which: 106
+  }, {
+    x: 0,
+    y: 0,
+    key: 'v',
+    which: 118
+  }, {
+    x: 1,
+    y: 0,
+    key: 'b',
+    which: 98
+  }, {
+    x: 2,
+    y: 0,
+    key: 'n',
+    which: 110
+  }, {
+    x: 3,
+    y: 0,
+    key: 'm',
+    which: 109
+  }];
+  var legalKeysWhich = vm.legalKeys.map(function (x) {
+    return x.which;
+  });
+  var songShell = {
+    "_version": "1.0.0",
+    "_beatsPerMinute": 120,
+    "_beatsPerBar": 16,
+    "_noteJumpSpeed": 10,
+    "_shuffle": 0,
+    "_shufflePeriod": 0.5,
+    "_events": [],
+    "_notes": [],
+    "_obstacles": []
+  };
 
   vm.onKeypress = function (e) {
     console.log(e.which);
@@ -36452,9 +36544,13 @@ app.controller('MainController', ['$scope', function ($scope) {
       return;
     }
 
-    if (e.which === 98) {
+    if (legalKeysWhich.includes(e.which)) {
+      var item = vm.legalKeys.filter(function (x) {
+        return x.which === e.which;
+      })[0];
+      vm.noteAnimation(item);
       vm.notes.push({
-        which: e.which,
+        position: item,
         time: Date.now()
       });
     }
@@ -36471,8 +36567,29 @@ app.controller('MainController', ['$scope', function ($scope) {
     vm.convertNotes(vm.notes);
     vm.startTime = null;
     console.log(vm.notes);
-    vm.notesJson = JSON.stringify(vm.notes);
+    songShell._beatsPerMinute = vm.bpm;
+    songShell._notes = vm.notes;
+    vm.notesJson = JSON.stringify(songShell);
     vm.notes = [];
+  };
+
+  vm.noteAnimation = function (item) {
+    return $q(function (resolve) {
+      if (item.cancel) {
+        $timeout.cancel(item.cancel);
+        item.css = '';
+        item.cancel = null;
+      }
+
+      $timeout(function () {
+        return item.css = "ping-".concat(item.x > 1 ? 'blue' : 'red');
+      }, 1);
+      item.cancel = $timeout(function () {
+        item.css = '';
+        item.cancel = null;
+      }, 500);
+      resolve();
+    });
   };
 
   vm.convertNotes = function (notes) {
@@ -36494,14 +36611,17 @@ app.controller('MainController', ['$scope', function ($scope) {
       var part = Math.floor(time % millisecondsBetweenBeats / millisecondsBetweenQuantums);
       var newNote = new _note.BeatNote();
       newNote._time = beat + part * vm.precision;
-      newNote._type = index % 2;
-      newNote._lineIndex = index % 2 * 4;
+      newNote._type = note.position.x > 1 ? 1 : 0;
+      newNote._lineIndex = note.position.x;
+      newNote._lineLayer = note.position.y;
       return newNote;
     });
   };
 
-  $scope.onAudioChange = function (src) {
+  $scope.onAudioChange = function (src, name) {
     $scope.audioPlayer[0].src = src;
+    vm.audioSrcName = name;
+    $scope.$digest();
   };
 }]);
 
@@ -36511,8 +36631,9 @@ _angular.default.module('bstm').directive('bstmAudio', [function () {
     link: function link($scope, element, attrs) {
       element.on('change', function () {
         if (this.files[0]) {
+          console.log($scope.audioSrcName);
           var file = URL.createObjectURL(this.files[0]);
-          $scope.onAudioChange(file);
+          $scope.onAudioChange(file, this.files[0].name);
         }
       });
     }
@@ -36554,7 +36675,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59408" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49639" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
